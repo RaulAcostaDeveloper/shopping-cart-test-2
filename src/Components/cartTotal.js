b51128a779334cc83c1d69db19bf9d3878a1edcb
Raúl Acosta <raulacostadeveloper@outlook.com>
2024-06-22 20:11:52 -0600

    Toda la funcionalidad (En crudo y sin re factorizar)

--- src/Components/cartTotal.js
+++ src/Components/cartTotal.js
@@ -1,10 +1,33 @@
+'use client'
+import { ProductsContext } from "@/productsController"
+import Link from "next/link"
+import { useContext, useEffect, useState } from "react"
+
 export const CartTotal = () => {
-    // Pendiente trabajar en el total
+    const [total, setTotal] = useState(0)
+    const { cartState, modificarCarrito } = useContext(ProductsContext)
+    useEffect(() => {
+        let contador = 0;
+        cartState.forEach(element => {
+            contador += element.price * element.stock;
+        });
+        setTotal(contador.toFixed(2));
+    }, [cartState]);
+
+    const handleCheckout = () => {
+        // Supongo que el comportamiento que se espera es limpiar el carrito y regresar a index
+        console.log('Limpiando el carrito ');
+        cartState.forEach(element => {
+            modificarCarrito(element.code, element.size, 0, "elimina");
+        });
+    }
     return (
         <div className="border-solid border">
             <h3>Order Summary</h3>
-            <span>Subtotal: </span>
-            <button>Checkout</button>
+            <span>Subtotal: $ {total}</span>
+            <Link href={'/'} onClick={handleCheckout}>
+                <button>Checkout</button>
+            </Link>
         </div>
     )
 }
